2fc21c9fd8128f3056e4e4419aad690d3fafa0d4
Naman Binu <namanbinu356@gmail.com>
2025-01-26 15:09:28 +0530

    assignment completed

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const { resolve } = require('path');
+require('dotenv').config();
 
-const app = express();
-const port = 3010;
+const isAdmin = process.env.IS_ADMIN === 'true';
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
